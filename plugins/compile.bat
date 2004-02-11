bd2ef13798edfc67728159691096a4c4453923bc
Felix Geyer <SniperBeamer>
2004-02-11 19:32:59 +0000

    updated header | added compiler parameter -"^"

--- plugins/compile.bat
+++ plugins/compile.bat
@@ -1,8 +1,13 @@
+rem AMX Mod X
+rem
+rem (c) 2002-2004, OLO
+rem  modified by the AMX Mod X Development Team
+
 @echo off
 
 if not exist compiled mkdir compiled
 if exist temp.txt del temp.txt
-for %%i in (*.sma) do sc %%i -ocompiled\%%i >> temp.txt
+for %%i in (*.sma) do sc %%i -ocompiled\%%i -"^" >> temp.txt
 copy compiled\*.sma compiled\*.amx
 del compiled\*.sma
 cls

476c788c51d60ca081ac2bebfb5092920da8ffe7
Felix Geyer <SniperBeamer>
2004-02-15 02:33:49 +0000

    added header

--- plugins/compile.sh
+++ plugins/compile.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#AMX Mod X
+#
+# (c) 2002-2004, OLO
+#  modified by the AMX Mod X Development Team
+
 test -e compiled || mkdir compiled
 ls *.sma | xargs -i ./sc \{\} -ocompiled/\{\} > temp.txt 
 ls compiled/*.sma | xargs -i basename \{\} .sma | xargs -i mv compiled/\{\}.sma compiled/\{\}.amx

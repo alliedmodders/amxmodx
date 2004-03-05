0b492c59f33915e8de6d4a61d78d2ca34235d21b
Felix Geyer <SniperBeamer>
2004-03-05 21:04:17 +0000

    changed header | updated url

--- amxmodx/meta_api.cpp
+++ amxmodx/meta_api.cpp
@@ -1,32 +1,32 @@
-/*
-* Copyright (c) 2002-2003 Aleksander Naszko
+/* AMX Mod X
 *
-*    This file is part of AMX Mod.
+* by the AMX Mod X Development Team
+*  originally developed by OLO
 *
-*    AMX Mod is free software; you can redistribute it and/or modify it
-*    under the terms of the GNU General Public License as published by the
-*    Free Software Foundation; either version 2 of the License, or (at
-*    your option) any later version.
 *
-*    AMX Mod is distributed in the hope that it will be useful, but
-*    WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-*    General Public License for more details.
+*  This program is free software; you can redistribute it and/or modify it
+*  under the terms of the GNU General Public License as published by the
+*  Free Software Foundation; either version 2 of the License, or (at
+*  your option) any later version.
 *
-*    You should have received a copy of the GNU General Public License
-*    along with AMX Mod; if not, write to the Free Software Foundation,
-*    Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*  This program is distributed in the hope that it will be useful, but
+*  WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+*  General Public License for more details.
 *
-*    In addition, as a special exception, the author gives permission to
-*    link the code of this program with the Half-Life Game Engine ("HL
-*    Engine") and Modified Game Libraries ("MODs") developed by Valve,
-*    L.L.C ("Valve").  You must obey the GNU General Public License in all
-*    respects for all of the code used other than the HL Engine and MODs
-*    from Valve.  If you modify this file, you may extend this exception
-*    to your version of the file, but you are not obligated to do so.  If
-*    you do not wish to do so, delete this exception statement from your
-*    version.
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software Foundation,
+*  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 *
+*  In addition, as a special exception, the author gives permission to
+*  link the code of this program with the Half-Life Game Engine ("HL
+*  Engine") and Modified Game Libraries ("MODs") developed by Valve,
+*  L.L.C ("Valve"). You must obey the GNU General Public License in all
+*  respects for all of the code used other than the HL Engine and MODs
+*  from Valve. If you modify this file, you may extend this exception
+*  to your version of the file, but you are not obligated to do so. If
+*  you do not wish to do so, delete this exception statement from your
+*  version.
 */
 
 #include <extdll.h>
@@ -39,8 +39,8 @@ plugin_info_t Plugin_info = {
   AMX_VERSION,  // version
   __DATE__, // date
   "AMX Mod X Dev Team", // author
-  "http://www.amxmod.info",  // url
-  "AMX",  // logtag
+  "http://www.amxmodx.org",  // url
+  "AMXX",  // logtag
   PT_ANYTIME,// (when) loadable
   PT_ANYTIME,// (when) unloadable
 };

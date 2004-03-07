745e41ce3b39e38d71518059856eb49b98c7c471
Pavol Marko <pavolmarko@gmail.com>
2004-03-07 19:28:30 +0000

    using __linux__ instead of LINUX

--- dlls/cstrike/cstrike.cpp
+++ dlls/cstrike/cstrike.cpp
@@ -1315,7 +1315,7 @@ C_DLLEXPORT int Meta_Detach(PLUG_LOADTIME now, PL_UNLOAD_REASON reason) {
 	}
 }
 
-#ifdef LINUX
+#ifdef __linux__
 C_DLLEXPORT void GiveFnptrsToDll( enginefuncs_t* pengfuncsFromEngine, globalvars_t *pGlobals ) {
 #else
 void WINAPI GiveFnptrsToDll( enginefuncs_t* pengfuncsFromEngine, globalvars_t *pGlobals ) {

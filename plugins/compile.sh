30ca7209205938714e0f100552fb1c081124a6ce
Felix Geyer <SniperBeamer>
2004-07-24 00:36:17 +0000

    changed file extension to .amxx

--- plugins/compile.sh
+++ plugins/compile.sh
@@ -8,7 +8,7 @@
 # This file is part of AMX Mod X.
 
 test -e compiled || mkdir compiled
-ls *.sma | xargs -i ./sc \{\} -ocompiled/\{\} > temp.txt 
-ls compiled/*.sma | xargs -i basename \{\} .sma | xargs -i mv compiled/\{\}.sma compiled/\{\}.amx
+ls *.sma | xargs -i ./amxxsc \{\} -ocompiled/\{\} > temp.txt 
+ls compiled/*.sma | xargs -i basename \{\} .sma | xargs -i mv compiled/\{\}.sma compiled/\{\}.amxx
 more temp.txt
 rm temp.txt

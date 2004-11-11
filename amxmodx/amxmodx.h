57607f1a28c79c360f9ef7cc707a1f34f7518250
David Anderson <dvander@alliedmods.net>
2004-11-11 11:49:38 +0000

    *** empty log message ***

--- amxmodx/amxmodx.h
+++ amxmodx/amxmodx.h
@@ -66,7 +66,7 @@
 #include "amxxlog.h"
 
 #define AMXXLOG_Log g_log.Log
-#define AMX_VERSION     "1.0"
+#define AMX_VERSION     "1.00"
 
 extern AMX_NATIVE_INFO  core_Natives[];
 extern AMX_NATIVE_INFO  time_Natives[];

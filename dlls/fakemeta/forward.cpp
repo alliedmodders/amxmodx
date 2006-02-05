93ad26fb2a677335b0df37707b1d9689a4924c37
David Anderson <dvander@alliedmods.net>
2006-02-05 08:25:45 +0000

    oops, forgot there was a sep. def for this

--- dlls/fakemeta/forward.cpp
+++ dlls/fakemeta/forward.cpp
@@ -86,7 +86,7 @@ void SetModel(edict_t *e, const char *m)
 }
 void SetModel_post(edict_t *e, const char *m)
 {
-	FM_ENG_HANDLE(FM_SetModel, (EnginePost[FM_SetModel].at(i), ENTINDEX(e), m));
+	FM_ENG_HANDLE_POST(FM_SetModel, (EnginePost[FM_SetModel].at(i), ENTINDEX(e), m));
 	RETURN_META(MRES_IGNORED);
 }
 
@@ -153,7 +153,7 @@ void AlertMessage_post(ALERT_TYPE atype, char *szFmt, ...)
 	va_start(ap, szFmt);
 	vsprintf(buf, szFmt, ap);
 	va_end(ap);
-	FM_ENG_HANDLE(FM_AlertMessage, (EnginePost[FM_AlertMessage].at(i), atype, buf));
+	FM_ENG_HANDLE_POST(FM_AlertMessage, (EnginePost[FM_AlertMessage].at(i), atype, buf));
 	RETURN_META(MRES_IGNORED);
 }
 
@@ -187,7 +187,7 @@ void VecToAngles_post(const float *rgflVectorIn, float *rgflVectorOut)
 	cell vec2[3] = {amx_ftoc(rgflVectorOut[0]), amx_ftoc(rgflVectorOut[1]), amx_ftoc(rgflVectorOut[2])};;
 	cell retvec1 = MF_PrepareCellArray(vec1, 3);
 	cell retvec2 = MF_PrepareCellArray(vec2, 3);
-	FM_ENG_HANDLE(FM_VecToAngles, (EnginePost[FM_VecToAngles].at(i),  retvec1, retvec2));
+	FM_ENG_HANDLE_POST(FM_VecToAngles, (EnginePost[FM_VecToAngles].at(i),  retvec1, retvec2));
 	RETURN_META(MRES_IGNORED);
 }
 
@@ -203,7 +203,7 @@ void MoveToOrigin_post(edict_t *ent, const float *pflGoal, float dist, int iMove
 {
 	cell vec[3] = {amx_ftoc(pflGoal[0]), amx_ftoc(pflGoal[1]), amx_ftoc(pflGoal[2])};
 	cell retvec = MF_PrepareCellArray(vec, 3);
-	FM_ENG_HANDLE(FM_MoveToOrigin, (EnginePost[FM_MoveToOrigin].at(i),  ENTINDEX(ent), retvec, dist, iMoveType));
+	FM_ENG_HANDLE_POST(FM_MoveToOrigin, (EnginePost[FM_MoveToOrigin].at(i),  ENTINDEX(ent), retvec, dist, iMoveType));
 	RETURN_META(MRES_IGNORED);
 }
 
@@ -215,7 +215,7 @@ edict_t *FindEntityByString(edict_t *pEdictStartSearchAfter, const char *pszFiel
 
 edict_t *FindEntityByString_post(edict_t *pEdictStartSearchAfter, const char *pszField, const char *pszValue)
 {
-	FM_ENG_HANDLE(FM_FindEntityByString, (EnginePost[FM_FindEntityByString].at(i),  ENTINDEX(pEdictStartSearchAfter), pszField, pszValue));
+	FM_ENG_HANDLE_POST(FM_FindEntityByString, (EnginePost[FM_FindEntityByString].at(i),  ENTINDEX(pEdictStartSearchAfter), pszField, pszValue));
 	RETURN_META_VALUE(MRES_IGNORED, INDEXENT2((int)mlCellResult));
 }
 // pfnGetEntityIllum

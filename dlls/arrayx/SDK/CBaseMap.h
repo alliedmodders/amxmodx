4cb8d4adc7a34f56bdc6188ddd42bcc3f8dbcf19
Twilight Suzuka <rukia>
2006-03-14 02:54:54 +0000

    *** empty log message ***

---
+++ dlls/arrayx/SDK/CBaseMap.h
@@ -0,0 +1,28 @@
+#ifndef _BASE_MAPCLASS_H
+#define _BASE_MAPCLASS_H
+
+#include "JudyIncludes.h"
+
+class CBaseMap
+{
+public:
+	virtual Word_t Clear() =0;
+	virtual Word_t MemoryUsed() =0;
+
+	virtual int Delete(char* Key) =0;
+
+	virtual void Set(char* Index, Pvoid_t value, bool disable_check = false) =0;
+	
+	virtual Pvoid_t Get(char* Index, bool disable_check = false) =0;
+
+	virtual char* First(char* Start = "") =0;
+	virtual char* Next(char* Start) =0;
+	virtual char* Prev(char* Start) =0;
+	virtual char* Last(char* Start) =0;
+
+	virtual bool IsFilled(char* Index) =0; 
+	virtual bool IsEmpty(char* Index) =0;
+};
+
+#endif
+

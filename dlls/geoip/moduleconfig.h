4b0b3c0c7cada6d029650d8bb8df005070512206
Borja Ferrer <faluco@alliedmods.net>
2006-03-20 22:58:27 +0000

    bumped version

--- dlls/geoip/moduleconfig.h
+++ dlls/geoip/moduleconfig.h
@@ -5,7 +5,7 @@
 
 // Module info
 #define MODULE_NAME "GeoIP"
-#define MODULE_VERSION "1.70"
+#define MODULE_VERSION "1.71"
 #define MODULE_AUTHOR "AMX Mod X Dev Team"
 #define MODULE_URL "http://www.amxmodx.org/"
 #define MODULE_LOGTAG "GEOIP"
--- dlls/ns/ns/moduleconfig.h
+++ dlls/ns/ns/moduleconfig.h
@@ -4,7 +4,7 @@
 
 // Module info
 #define MODULE_NAME "NS"
-#define MODULE_VERSION "1.70"
+#define MODULE_VERSION "1.71"
 #define MODULE_AUTHOR "Steve Dudenhoeffer"
 #define MODULE_URL "http://www.amxmodx.org/"
 #define MODULE_LOGTAG "NS"

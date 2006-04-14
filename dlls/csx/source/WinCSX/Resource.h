28b04878cef492e8b3925e4ea804c9523cf25c1d
Scott Ehlert <ds@alliedmods.net>
2006-04-14 15:55:25 +0000

    Added visual style support :o

--- dlls/csx/source/WinCSX/Resource.h
+++ dlls/csx/source/WinCSX/Resource.h
@@ -3,16 +3,17 @@
 // Used by WinCSX.rc
 //
 #define IDC_MYICON                      2
+#define IDM_VISUALSTYLES                1
 #define IDD_WINCSX_DIALOG               102
 #define IDS_APP_TITLE                   103
 #define IDD_WINCSXBOX                   103
 #define IDM_ABOUT                       104
-#define IDM_EXIT                        105
 #define IDI_WINCSX                      107
 #define IDI_SMALL                       108
 #define IDC_WINCSX                      109
 #define IDR_MAINFRAME                   128
 #define IDD_ABOUTBOX                    129
+#define IDR_241                         132
 #define IDC_LIST                        1010
 #define IDC_BUTTON_ABOUT                1029
 #define IDC_ABOUT                       1029
@@ -36,7 +37,6 @@
 #define IDC_AUTHOR                      1114
 #define ID_HELP_DIALOG                  32771
 #define IDM_FILE_DIALOG                 32773
-#define IDM_WINCSX                      32774
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -44,7 +44,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32775
 #define _APS_NEXT_CONTROL_VALUE         1033
 #define _APS_NEXT_SYMED_VALUE           110

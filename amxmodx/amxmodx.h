8fec829c638a18240b3cd6a7b7e3171f4097bd3f
Scott Ehlert <ds@alliedmods.net>
2006-05-06 04:49:36 +0000

    Version bump in AMX_VERSION define (since version.rc was bumped)

--- amxmodx/amxmodx.h
+++ amxmodx/amxmodx.h
@@ -73,7 +73,7 @@
 
 #define AMXXLOG_Log g_log.Log
 #define AMXXLOG_Error g_log.LogError
-#define AMX_VERSION	"1.71"
+#define AMX_VERSION	"1.72"
 
 extern AMX_NATIVE_INFO core_Natives[];
 extern AMX_NATIVE_INFO time_Natives[];

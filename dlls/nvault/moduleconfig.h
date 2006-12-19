13e619bf97c69d506ccabddaff9d280c37e64288
David Anderson <dvander@alliedmods.net>
2006-12-19 14:18:05 +0000

    bumped versions

--- dlls/nvault/moduleconfig.h
+++ dlls/nvault/moduleconfig.h
@@ -5,7 +5,7 @@
 
 // Module info
 #define MODULE_NAME "nVault"
-#define MODULE_VERSION "1.76"
+#define MODULE_VERSION "1.76c"
 #define MODULE_AUTHOR "AMX Mod X Dev Team"
 #define MODULE_URL "http://www.amxmodx.org/"
 #define MODULE_LOGTAG "nVault"

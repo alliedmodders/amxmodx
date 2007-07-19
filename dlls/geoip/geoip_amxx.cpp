4b02ffa920abd0f2bdf2bf8e8cae4a28838ef6ac
Steve Dudenhoeffer <stevedude@gmail.com>
2007-07-19 18:37:13 +0000

    Fix for amb519: geoip_code2 and geoip_code3 will overflow the result buffer on an unsuccessful lookup.
    Added two replacement natives for those two: geoip_code2_ex and geoip_code3_ex, could not modify the old natives without breaking backwards compatibility.

--- dlls/geoip/geoip_amxx.cpp
+++ dlls/geoip/geoip_amxx.cpp
@@ -28,6 +28,39 @@ static cell AMX_NATIVE_CALL amx_geoip_code3(AMX *amx, cell *params)
 	const char *ccode = GeoIP_country_code3_by_addr(gi, ip);
 	return MF_SetAmxString(amx, params[2], ccode?ccode:"error", 4);
 }
+static cell AMX_NATIVE_CALL amx_geoip_code2_ex(AMX *amx, cell *params)
+{
+	int len = 0;
+	char *ip = MF_GetAmxString(amx, params[1], 0, &len);
+	StripPort(ip);
+	const char *ccode = GeoIP_country_code_by_addr(gi, ip);
+	
+	if (ccode == NULL)
+	{
+		return 0;
+	}
+	
+	MF_SetAmxString(amx, params[2], ccode, 2);
+	
+	return 1;
+}
+
+static cell AMX_NATIVE_CALL amx_geoip_code3_ex(AMX *amx, cell *params)
+{
+	int len = 0;
+	char *ip = MF_GetAmxString(amx, params[1], 0, &len);
+	StripPort(ip);
+	const char *ccode = GeoIP_country_code3_by_addr(gi, ip);
+
+	if (ccode == NULL)
+	{
+		return 0;
+	}
+	
+	MF_SetAmxString(amx, params[2], ccode, 3);
+	
+	return 1;
+}
 
 static cell AMX_NATIVE_CALL amx_geoip_country(AMX *amx, cell *params)
 {
@@ -60,6 +93,10 @@ void OnAmxxDetach()
 AMX_NATIVE_INFO geoip_natives[] = {
 	{"geoip_code2",		amx_geoip_code2},
 	{"geoip_code3",		amx_geoip_code3},
+	
+	{"geoip_code2_ex",	amx_geoip_code2_ex},
+	{"geoip_code3_ex",	amx_geoip_code3_ex},
+	
 	{"geoip_country",	amx_geoip_country},
 	{NULL,				NULL},
 };

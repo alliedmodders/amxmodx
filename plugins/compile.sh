7ae802d73653fd53307998fef7297fd6200b02db
Scott Ehlert <ds@alliedmods.net>
2014-05-18 20:37:44 -0500

    Fix compile.sh script for OS X.

--- plugins/compile.sh
+++ plugins/compile.sh
@@ -12,18 +12,11 @@
 test -e compiled || mkdir compiled
 rm -f temp.txt
 
-# Choose compiler binary
-if test `uname` = "Darwin"; then
-	pc=./amxxpc_osx
-else
-	pc=./amxxpc
-fi
-
 for sourcefile in *.sma
 do
         amxxfile="`echo $sourcefile | sed -e 's/\.sma$/.amxx/'`"
         echo -n "Compiling $sourcefile ..."
-        $pc $sourcefile -ocompiled/$amxxfile >> temp.txt
+        ./amxxpc $sourcefile -ocompiled/$amxxfile >> temp.txt
         echo "done"
 done
 

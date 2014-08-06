8d2770f2f254260a0acc5b978ac87f077b6a733c
Arkshine <hv.contact@gmail.com>
2014-08-07 00:43:29 +0200

    Put back original SM headers.

--- public/ITextParsers.h
+++ public/ITextParsers.h
@@ -1,12 +1,32 @@
-/** 
- * vim: set ts=4 sw=4 tw=99 noet:
+/**
+ * vim: set ts=4 :
+ * =============================================================================
+ * SourceMod
+ * Copyright (C) 2004-2008 AlliedModders LLC.  All rights reserved.
+ * =============================================================================
  *
- * AMX Mod X, based on AMX Mod by Aleksander Naszko ("OLO").
- * Copyright (C) The AMX Mod X Development Team.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 3.0, as published by the
+ * Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
  *
- * This software is licensed under the GNU General Public License, version 3 or higher.
- * Additional exceptions apply. For full license details, see LICENSE.txt or visit:
- *     https://alliedmods.net/amxmodx-license
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * As a special exception, AlliedModders LLC gives you permission to link the
+ * code of this program (as well as its derivative works) to "Half-Life 2," the
+ * "Source Engine," the "SourcePawn JIT," and any Game MODs that run on software
+ * by the Valve Corporation.  You must obey the GNU General Public License in
+ * all respects for all other code used.  Additionally, AlliedModders LLC grants
+ * this exception to all derivative works.  AlliedModders LLC defines further
+ * exceptions, found in LICENSE.txt (as of this writing, version JULY-31-2007),
+ * or <http://www.sourcemod.net/license.php>.
+ *
+ * Version: $Id$
  */
 
 #ifndef _INCLUDE_SOURCEMOD_TEXTPARSERS_INTERFACE_H_

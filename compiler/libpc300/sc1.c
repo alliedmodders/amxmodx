57b172072a3792cde09a5d784f20ca61cbc0868e
Arkshine <hv.contact@gmail.com>
2014-08-13 17:30:40 +0200

    Compiler: Fix bogus assert about name lengths.
    Imported from SM: https://bugs.alliedmods.net/show_bug.cgi?id=4486.

--- compiler/libpc300/sc1.c
+++ compiler/libpc300/sc1.c
@@ -4412,7 +4412,7 @@ static constvalue *insert_constval(constvalue *prev,constvalue *next,const char 
     error(103);       /* insufficient memory (fatal error) */
   memset(cur,0,sizeof(constvalue));
   if (name!=NULL) {
-    assert(strlen(name)<sNAMEMAX);
+    assert(strlen(name)<=sNAMEMAX);
     strcpy(cur->name,name);
   } /* if */
   cur->value=val;

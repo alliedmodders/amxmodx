62bf10784996577470d5ce99bc6f10395241da84
xPaw <github@xpaw.me>
2015-02-20 16:31:28 +0200

    Add features from SourceMod's compile.sh

--- plugins/compile.sh
+++ plugins/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 # AMX Mod X
 #
 # by the AMX Mod X Development Team
@@ -7,9 +7,7 @@
 #
 # This file is part of AMX Mod X.
 
-echo // AMX Mod X Batch Compiler
-echo // by the AMX Mod X Dev Team
-echo
+cd "$(dirname "$0")"
 
 if [ ! -f ./amxxpc ]; then
 	echo // Could not find amxxpc
@@ -18,11 +16,26 @@ fi
 
 test -e compiled || mkdir compiled
 
-for sourcefile in *.sma
-do
-	amxxfile="$(echo "$sourcefile" | sed -e 's/\.sma$/.amxx/')"
-
-	echo -n "// Compiling $sourcefile ..."
-
-	./amxxpc "$sourcefile" -ocompiled/"$amxxfile"
-done
+if [[ $# -ne 0 ]]; then
+	for sourcefile in "$@"; 
+	do
+		outfile="$(echo "$sourcefile" | sed -e 's/\.sma$/\.amxx/')";
+		echo -e "// Compiling $sourcefile...";
+		./amxxpc "$sourcefile" -ocompiled/"$outfile"
+		RETVAL=$?
+		if [ $RETVAL -ne 0 ]; then
+			exit 1;
+		fi
+	done
+else
+	for sourcefile in *.sp
+	do
+		outfile="$(echo "$sourcefile" | sed -e 's/\.sma$/\.amxx/')"
+		echo -e "// Compiling $sourcefile ..."
+		./amxxpc "$sourcefile" -ocompiled/"$outfile"
+		RETVAL=$?
+		if [ $RETVAL -ne 0 ]; then
+			exit 1;
+		fi
+	done
+fi

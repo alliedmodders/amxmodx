6d6a1c1ab0e6b2af6810375436b792d29de88ba0
Arkshine <hv.contact@gmail.com>
2015-07-29 22:20:16 +0200

    Add a data argument in INI/SMC_ParseFile() to pass through to callbacks (bug 6228)

--- amxmodx/textparse.h
+++ amxmodx/textparse.h
@@ -30,6 +30,7 @@ public:
 		end_section = -1;
 		raw_line    = -1;
 		handle      = -1;
+		data        = 0;
 	}
 
 public:
@@ -41,19 +42,19 @@ public:
 	void ReadSMC_ParseStart()
 	{
 		if (parse_start != -1)	
-			executeForwards(parse_start, handle);
+			executeForwards(parse_start, handle, data);
 	}
 
 	void ReadSMC_ParseEnd(bool halted, bool failed)
 	{
 		if (parse_end != -1)	
-			executeForwards(parse_end, handle, halted ? 1 : 0, failed ? 1 : 0);
+			executeForwards(parse_end, handle, halted ? 1 : 0, failed ? 1 : 0, data);
 	}
 
 	SMCResult ReadSMC_NewSection(const SMCStates *states, const char *name)
 	{
 		if (new_section != -1)
-			return (SMCResult)executeForwards(new_section, handle, name);
+			return (SMCResult)executeForwards(new_section, handle, name, data);
 
 		return SMCResult_Continue;
 	}
@@ -61,7 +62,7 @@ public:
 	SMCResult ReadSMC_KeyValue(const SMCStates *states, const char *key, const char *value)
 	{
 		if (key_value != -1)
-			return (SMCResult)executeForwards(key_value, handle, key, value);
+			return (SMCResult)executeForwards(key_value, handle, key, value, data);
 
 		return SMCResult_Continue;
 	}
@@ -69,7 +70,7 @@ public:
 	SMCResult ReadSMC_LeavingSection(const SMCStates *states)
 	{
 		if (end_section != -1)
-			return (SMCResult)executeForwards(end_section, handle);
+			return (SMCResult)executeForwards(end_section, handle, data);
 
 		return SMCResult_Continue;
 	}
@@ -77,7 +78,7 @@ public:
 	SMCResult ReadSMC_RawLine(const SMCStates *states, const char *line)
 	{
 		if (raw_line != -1)
-			return (SMCResult)executeForwards(raw_line, handle, line, states->line);
+			return (SMCResult)executeForwards(raw_line, handle, line, states->line, data);
 
 		return SMCResult_Continue;
 	}
@@ -90,19 +91,19 @@ public:
 	void ReadINI_ParseStart()
 	{
 		if (parse_start != -1)
-			executeForwards(parse_start, handle);
+			executeForwards(parse_start, handle, data);
 	}
 
 	void ReadINI_ParseEnd(bool halted)
 	{
 		if (parse_end != -1)
-			executeForwards(parse_end, handle, halted ? 1 : 0);
+			executeForwards(parse_end, handle, halted ? 1 : 0, data);
 	}
 
 	bool ReadINI_NewSection(const char *section, bool invalid_tokens, bool close_bracket, bool extra_tokens, unsigned int *curtok)
 	{
 		if (new_section != -1)
-			return executeForwards(new_section, handle, section, invalid_tokens, close_bracket, extra_tokens, *curtok) > 0 ? true : false;
+			return executeForwards(new_section, handle, section, invalid_tokens, close_bracket, extra_tokens, *curtok, data) > 0 ? true : false;
 
 		return true;
 	}
@@ -110,7 +111,7 @@ public:
 	bool ReadINI_KeyValue(const char *key, const char *value, bool invalid_tokens, bool equal_token, bool quotes, unsigned int *curtok)
 	{
 		if (key_value != -1)
-			return executeForwards(key_value, handle, key, value, invalid_tokens, equal_token, quotes, *curtok) > 0 ? true : false;
+			return executeForwards(key_value, handle, key, value, invalid_tokens, equal_token, quotes, *curtok, data) > 0 ? true : false;
 
 		return true;
 	}
@@ -118,7 +119,7 @@ public:
 	bool ReadINI_RawLine(const char *line, unsigned int *curtok)
 	{
 		if (raw_line != -1)
-			return executeForwards(raw_line, handle, line, *curtok) > 0 ? true : false;
+			return executeForwards(raw_line, handle, line, *curtok, data) > 0 ? true : false;
 
 		return true;
 	}
@@ -130,6 +131,7 @@ public:
 	int end_section;
 	int raw_line;
 	int handle;
+	cell data;
 };
 
 extern NativeHandle<ParseInfo> TextParsersHandles;

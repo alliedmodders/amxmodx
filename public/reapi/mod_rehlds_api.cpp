da28fafadd0f0f3ad5ab0633e6a61ad7367e9dd6
Arkshine <github@arkshine.me>
2017-02-28 22:16:53 +0100

    Add the necessary files to get ReHLDS interface

---
+++ public/reapi/mod_rehlds_api.cpp
@@ -0,0 +1,38 @@
+
+#include <amxmodx.h>
+#include "mod_rehlds_api.h"
+
+IRehldsApi*          RehldsApi;
+const RehldsFuncs_t* RehldsFuncs;
+IRehldsServerData*   RehldsData;
+IRehldsHookchains*   RehldsHookchains;
+IRehldsServerStatic* RehldsSvs;
+
+bool RehldsApi_Init()
+{
+#if defined(PLATFORM_WINDOWS)
+	auto library = "swds";
+#elif defined(PLATFORM_UNIX)
+	auto library = "engine_i486";
+#endif
+
+	if (!GET_IFACE<IRehldsApi>(library, RehldsApi, VREHLDS_HLDS_API_VERSION))
+	{
+		return false;
+	}
+
+	auto majorVersion = RehldsApi->GetMajorVersion();
+	auto minorVersion = RehldsApi->GetMinorVersion();
+
+	if (majorVersion != REHLDS_API_VERSION_MAJOR || minorVersion < REHLDS_API_VERSION_MINOR)
+	{
+		return false;
+	}
+
+	RehldsFuncs      = RehldsApi->GetFuncs();
+	RehldsData       = RehldsApi->GetServerData();
+	RehldsHookchains = RehldsApi->GetHookchains();
+	RehldsSvs        = RehldsApi->GetServerStatic();
+
+	return true;
+}
---
+++ public/reapi/mod_rehlds_api.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "engine/rehlds_api.h"
+
+extern IRehldsApi* RehldsApi;
+extern const RehldsFuncs_t* RehldsFuncs;
+extern IRehldsServerData* RehldsData;
+extern IRehldsHookchains* RehldsHookchains;
+extern IRehldsServerStatic* RehldsSvs;
+
+extern bool RehldsApi_Init();

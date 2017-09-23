6b87776946dc46ed7ce57c9120355527ae33549a
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2017-09-24 01:03:53 +0900

    Added some features

---
+++ modules/ts/tse/dllfuncs.h
@@ -0,0 +1,20 @@
+#include "amxxmodule.h"
+#include "memory.h"
+#pragma once
+
+// AMX Mod X, based on AMX Mod by Aleksander Naszko ("OLO").
+// Copyright (C) The AMX Mod X Development Team.
+// Copyright (C) 2017, SNMetamorph.
+//
+// This software is licensed under the GNU General Public License, version 3 or higher.
+// Additional exceptions apply. For full license details, see LICENSE.txt or visit:
+//     https://alliedmods.net/amxmodx-license
+
+// Platform-specific inclusions
+#if defined(__linux__)
+
+#else
+	#include <windows.h>
+#endif
+
+
---
+++ modules/ts/tse/memory.h
@@ -0,0 +1,11 @@
+#include "amxxmodule.h"
+#pragma once
+
+// AMX Mod X, based on AMX Mod by Aleksander Naszko ("OLO").
+// Copyright (C) The AMX Mod X Development Team.
+// Copyright (C) 2017, SNMetamorph.
+//
+// This software is licensed under the GNU General Public License, version 3 or higher.
+// Additional exceptions apply. For full license details, see LICENSE.txt or visit:
+//     https://alliedmods.net/amxmodx-license
+

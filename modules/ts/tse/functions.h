bc8e68b4d3c6de1b22bdf078d233be28a3b479f0
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2017-09-26 22:09:58 +0900

    Fixed license header in source/header files

--- modules/ts/tse/functions.h
+++ modules/ts/tse/functions.h
@@ -1,17 +1,19 @@
+// vim: set ts=4 sw=4 tw=99 noet:
+//
+// AMX Mod X, based on AMX Mod by Aleksander Naszko ("OLO").
+// Copyright (C) The AMX Mod X Development Team.
+// Copyright (C) 2017 SNMetamorph.
+//
+// This software is licensed under the GNU General Public License, version 3 or higher.
+// Additional exceptions apply. For full license details, see LICENSE.txt or visit:
+//     https://alliedmods.net/amxmodx-license
+
 #include "amxxmodule.h"
 #include "msghooking.h"
 #include "weapinfo.h"
 #include "CPlayer.h"
 #include "misc.h"
 #pragma once
-
-// AMX Mod X, based on AMX Mod by Aleksander Naszko ("OLO").
-// Copyright (C) The AMX Mod X Development Team.
-// Copyright (C) 2017, SNMetamorph.
-//
-// This software is licensed under the GNU General Public License, version 3 or higher.
-// Additional exceptions apply. For full license details, see LICENSE.txt or visit:
-//     https://alliedmods.net/amxmodx-license
 
 static cell AMX_NATIVE_CALL tse_getuserslots(AMX *amx, cell *params) 
 {
--- modules/ts/tse/memory.cpp
+++ modules/ts/tse/memory.cpp
@@ -1,13 +1,15 @@
-#include "amxxmodule.h"
-#include "memory.h"
-
+// vim: set ts=4 sw=4 tw=99 noet:
+//
 // AMX Mod X, based on AMX Mod by Aleksander Naszko ("OLO").
 // Copyright (C) The AMX Mod X Development Team.
-// Copyright (C) 2017, SNMetamorph.
+// Copyright (C) 2017 SNMetamorph.
 //
 // This software is licensed under the GNU General Public License, version 3 or higher.
 // Additional exceptions apply. For full license details, see LICENSE.txt or visit:
 //     https://alliedmods.net/amxmodx-license
+
+#include "amxxmodule.h"
+#include "memory.h"
 
 libwrapper gamelib;
 bool FindGameLibAddress()

0e209c90bcba089a6ae0109a3688faf4482c7656
Arkshine <github@arkshine.me>
2018-09-05 14:55:44 +0200

    Add Ham_SC_IsBreakable

--- modules/hamsandwich/hook_native.cpp
+++ modules/hamsandwich/hook_native.cpp
@@ -533,6 +533,7 @@ hook_t hooklist[] =
 	{ V("sc_isinworld",				Bool_Void) },
 	{ V("sc_isplayer",				Bool_Void) },
 	{ V("sc_isnetclient",			Bool_Void) },
+	{ V("sc_isbreakable",			Bool_Void) },
 	{ V("sc_canplaysequence",		Bool_Bool_Int) },
 	{ V("sc_canplaysentence2",		Bool_Bool) },
 	{ V("sc_playscriptedsentence",	Void_Str_Float_Float_Float_Bool_Cbase) },

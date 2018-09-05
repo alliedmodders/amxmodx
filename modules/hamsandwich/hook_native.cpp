65c4cd723aec018ae29dace418229c9b59c1672b
Arkshine <github@arkshine.me>
2018-09-05 14:41:43 +0200

    Add Ham_SC_ReflectGauss

--- modules/hamsandwich/hook_native.cpp
+++ modules/hamsandwich/hook_native.cpp
@@ -528,6 +528,7 @@ hook_t hooklist[] =
 	{ V("sc_issneaking",			Bool_Void) },
 	{ V("sc_isalive",				Bool_Void) },
 	{ V("sc_isbspmodel",			Bool_Void) },
+	{ V("sc_reflectgauss",			Bool_Void) },
 	{ V("sc_canplaysequence",		Bool_Bool_Int) },
 	{ V("sc_canplaysentence2",		Bool_Bool) },
 	{ V("sc_playscriptedsentence",	Void_Str_Float_Float_Float_Bool_Cbase) },

f8ec4c086de8bf265f23669373e773c018d528bf
CrXane <25283652+CrXane@users.noreply.github.com>
2020-06-05 21:29:28 -0400

    Added small features at compile.sh
    Ability to compile specific or multiple source files using compile.sh

--- plugins/compile.sh
+++ plugins/compile.sh
@@ -7,18 +7,33 @@
 #
 # This file is part of AMX Mod X.
 
-# new code contributed by \malex\
+# to compile all source files in the directory
+#  execute the script without any parameters
+# to compile specific or multiple source files
+#  execute the script with parameters set as source files' name
+#  e.g ./compile.sh admin.sma admincmd.sma adminchat.sma
 
 test -e compiled || mkdir compiled
 rm -f temp.txt
 
-for sourcefile in *.sma
-do
-        amxxfile="`echo $sourcefile | sed -e 's/\.sma$/.amxx/'`"
-        echo -n "Compiling $sourcefile ..."
-        ./amxxpc $sourcefile -ocompiled/$amxxfile >> temp.txt
-        echo "done"
-done
+if [ $# != 0 ]
+	then
+		for src in "$@"
+		do
+			amxxfile="`echo $src | sed -e 's/\.sma$/.amxx/'`"
+			echo -n "Compiling $src ..."
+			./amxxpc $src -ocompiled/$amxxfile >> temp.txt
+			echo "done"
+		done
+	else
+		for sourcefile in *.sma
+		do
+			amxxfile="`echo $sourcefile | sed -e 's/\.sma$/.amxx/'`"
+			echo -n "Compiling $sourcefile ..."
+			./amxxpc $sourcefile -ocompiled/$amxxfile >> temp.txt
+			echo "done"
+		done
+fi
 
 less temp.txt
 rm temp.txt

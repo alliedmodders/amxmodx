eaab23f0068b34e295987486ed5ca89408326271
szGabu <162526684+szGabu@users.noreply.github.com>
2026-09-02 22:28:11 -0400

    Svengine Compatibility Fix (#1157)
    * feat: support regparm calling convention for Cvar_DirectSet
    
    * change: saferails for windows compilation, this is not needed in win32 build
    
    * add: gamedata for svengine engine functions
    
    * change: preprocessor directives at the start of the line

--- amxmodx/CvarManager.cpp
+++ amxmodx/CvarManager.cpp
@@ -17,7 +17,13 @@ CvarManager g_CvarManager;
 
 void (*Cvar_DirectSet_Actual)(struct cvar_s* var, const char *value) = nullptr;
 
-void Cvar_DirectSet_Custom(struct cvar_s *var, const char *value, IRehldsHook_Cvar_DirectSet *chain = nullptr)
+#if defined(__linux__)
+void (*Cvar_DirectSet_RegParm_Actual)(struct cvar_s* var, const char *value) __attribute__((regparm(3))) = nullptr;
+#else 
+#define Cvar_DirectSet_RegParm_Actual Cvar_DirectSet_Actual
+#endif
+
+void Cvar_DirectSet_Custom(struct cvar_s *var, const char *value, IRehldsHook_Cvar_DirectSet *chain = nullptr, bool useRegParm = false)
 {
 	CvarInfo* info = nullptr;
 
@@ -25,7 +31,7 @@ void Cvar_DirectSet_Custom(struct cvar_s *var, const char *value, IRehldsHook_Cv
 		|| strcmp(var->string, value) == 0                // Make sure old and new values are different to not trigger callbacks.
 		|| !g_CvarManager.CacheLookup(var->name, &info))  // No data in cache, nothing to do.
 	{
-		chain ? chain->callNext(var, value) : Cvar_DirectSet_Actual(var, value);
+		chain ? chain->callNext(var, value) : (useRegParm ? Cvar_DirectSet_RegParm_Actual(var, value) : Cvar_DirectSet_Actual(var, value));
 		return;
 	}
 
@@ -59,7 +65,7 @@ void Cvar_DirectSet_Custom(struct cvar_s *var, const char *value, IRehldsHook_Cv
 		oldValue = var->string;
 	}
 
-	chain ? chain->callNext(var, value) : Cvar_DirectSet_Actual(var, value);
+	chain ? chain->callNext(var, value) : (useRegParm ? Cvar_DirectSet_RegParm_Actual(var, value) : Cvar_DirectSet_Actual(var, value));
 
 	if (!info->binds.empty())
 	{
@@ -103,6 +109,13 @@ void Cvar_DirectSet_Custom(struct cvar_s *var, const char *value, IRehldsHook_Cv
 	}
 }
 
+#if defined(__linux__)
+void __attribute__((regparm(3))) Cvar_DirectSet_RegParm(struct cvar_s *var, const char *value)
+{
+    Cvar_DirectSet_Custom(var, value, nullptr, true);
+}
+#endif
+
 void Cvar_DirectSet(struct cvar_s *var, const char *value)
 {
 	Cvar_DirectSet_Custom(var, value);
@@ -137,7 +150,21 @@ void CvarManager::CreateCvarHook(void)
 		if (CommonConfig && CommonConfig->GetMemSig("Cvar_DirectSet", &functionAddress) && functionAddress)
 		{
 			// Disabled by default.
+
+#if defined(__linux__)
+			const char* raw = CommonConfig->GetKeyValue("RegParm");
+			bool useRegParm = raw ? (atoi(raw) != 0) : false; 
+			if(useRegParm)
+			{
+				m_HookDetour = DETOUR_CREATE_STATIC_FIXED(Cvar_DirectSet_RegParm, functionAddress);
+			}
+			else
+			{
+				m_HookDetour = DETOUR_CREATE_STATIC_FIXED(Cvar_DirectSet, functionAddress);
+			}
+#else 
 			m_HookDetour = DETOUR_CREATE_STATIC_FIXED(Cvar_DirectSet, functionAddress);
+#endif
 		}
 		else
 		{
